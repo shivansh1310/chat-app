0b1223d3f29e7f02855b32b7f045c363887a432f
unknown <dhimanhanu@gmail.com>
2026-04-15 19:01:55 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path"
+
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import { app, server } from "./lib/socket.js";
@@ -14,6 +17,7 @@ dotenv.config();
 app
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -25,6 +29,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes); //whenever we hit the auth rout we'd like to hit teh file associated with auth.route
 app.use("/api/messages", messageRoutes); 
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 server.listen(PORT, () => {
     console.log("Server running on PORT: " + PORT);
